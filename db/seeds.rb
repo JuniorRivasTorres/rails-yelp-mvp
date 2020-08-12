19cbf82fe04746ef049dc0e3539632841778c706
Junior Rivas <jrtimmobilier@gmail.com>
2020-08-12 15:51:07 -0400

    finish the routes ressources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create name: "go habs go", address: "rambo", phone_number: "88888", category: "japanese"
+Restaurant.create name: "go hab go", address: "rambo", phone_number: "88888", category: "italian"
+Restaurant.create name: " habs go", address: "rambo", phone_number: "88888", category: "japanese"
+Restaurant.create name: " habs", address: "rambo", phone_number: "88888", category: "chinese"
+Restaurant.create name: "go habs ", address: "rambo", phone_number: "88888", category: "italian" 
